7d4780458d67b1520dee91db2dc66f5d38ef8ed8
Kenny <kfernsten@gmail.com>
2013-05-16 18:22:10 -0600

    Answers to 2 of 3 problems

--- palinodrome_product.py
+++ palinodrome_product.py
@@ -1,3 +1,37 @@
 """A palindromic number reads the same both ways. The largest palindrome made
 from the product of two 2-digit numbers is 9009 = 91 99.Find the largest
 palindrome made from the product of two 3-digit numbers."""
+
+first = 100
+first_result = 0
+second_result = 0
+highest = 0
+
+#loop through first set of 3 digit numbers
+while first < 1000:
+    second = 100
+    #loop through second set of 3 digit numbers for every first set up numbers
+    while second < 1000:
+        result = first*second
+
+        #cast as string
+        string = str(result)
+
+        #split string in half
+        first_half, second_half = string[::2], string[1::2]
+
+        #flip second half
+        second_half = second_half[::-1]
+
+        #compare to see if they are the same
+        if first_half == second_half:
+            #if current value is higher than previous high
+            if result > highest:
+                highest = result
+                first_result = first
+                second_result = second
+        second += 1
+    first += 1
+
+print "The highest palindromic number is %s by %s * %s" % (highest, first_result, second_result)
+
--- series_product.py
+++ series_product.py
@@ -1,3 +1,36 @@
 """Complete this program to find the greatest produdct of five consecutive
 digits in the list"""
 in_file = open('array.txt')
+
+#read contents, and replace \n
+file_contents = in_file.read().replace('\n','')
+
+#Get length of file for while loop
+length = file_contents.__len__()
+
+
+position = 0
+
+highest_number = 0
+highest_chunk = None
+
+while position < length-4:
+    #get chunk of numbers
+    chunk = file_contents[0+position:5+position]
+
+    number = 1
+    #put each number into a list and iterate through
+    for i in list(chunk):
+        #cast number as an int and multiply against previous numbers (or 1 for first iteration)
+        number *= int(i)
+
+    #if newly multiplied number is higher than previous high, replace it
+    if number > highest_number:
+        highest_number = number
+        highest_chunk = chunk
+
+    #move the chunk window
+    position += 1
+
+print "The highest product is %s produced from chunk %s" % (highest_number, highest_chunk)
+
